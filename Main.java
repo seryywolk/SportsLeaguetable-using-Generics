36ec0f6adc64d5d30cfd4ec41fbb67cf95f0c720
Hayytov Serdar <91581576+seryywolk@users.noreply.github.com>
2022-05-08 01:44:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+package com.codewithserdar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create a generic class to implement a league table for a sport.
+        // The class should allow teams to be added to the list, and store
+        // a list of teams that belong to the league.
+        //
+        // Your class should have a method to print out the teams in order,
+        // with the team at the top of the league printed first.
+        //
+        // Only teams of the same type should be added to any particular
+        // instance of the league class - the program should fail to compile
+        // if an attempt is made to add an incompatible team.
+
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle, 2, 1 );
+
+        footballLeague.add(adelaideCrows);
+        footballLeague.add(melbourne);
+        footballLeague.add(hawthorn);
+        footballLeague.add(fremantle);
+
+//        footballLeague.add(baseballTeam);
+        footballLeague.showLeagueTable();
+
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team rawTeam = new Team("Raw Team");
+        rawTeam.addPlayer(beckham);
+        rawTeam.addPlayer(pat);
+
+        footballLeague.add(rawTeam);
+
+        League<Team> rawLeague = new League<>("Raw");
+        rawLeague.add(adelaideCrows);
+        rawLeague.add(baseballTeam);
+        rawLeague.add(rawTeam);
+
+        League reallyRaw = new League("Really Raw");
+        reallyRaw.add(adelaideCrows);
+        reallyRaw.add(baseballTeam);
+        reallyRaw.add(rawTeam);
+    }
+}
